ec4764d0dabf3ded14532971aaedf7ea4b5bbf2f
SirMatej <matej.mejsnar@seznam.cz>
2026-04-05 13:29:31 +0200

    Deleted duplicate of class Data (again)

--- CZ-lang/AppNotifierCZ.py
+++ CZ-lang/AppNotifierCZ.py
@@ -44,20 +44,6 @@ class Data:
                 return json.load(f)
         return {}
 
-class Data:
-    def __init__(self):
-        pass
-
-    def save(self, data):
-        with open(JSON, "w") as f:
-            json.dump(data, f)
-
-    def load(self):
-        if os.path.exists(JSON):
-            with open(JSON, "r") as f:
-                return json.load(f)
-        return {}
-
 DATA = Data().load()
 PICKED_ACCENT = DATA.get("PICKED_ACCENT", "Modrá")
 PICKED_BODY = DATA.get("PICKED_BODY", "Modrá") 
